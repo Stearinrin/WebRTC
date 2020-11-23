7e580b24f27f32911e6bab77b0c444ecb6b22e34
羽田野 湧太 <yuuta.hatano@maas.co.jp>
2020-11-23 11:07:13 +0900

    列車前面展望追加

--- js/main.js
+++ js/main.js
@@ -178,6 +178,7 @@ function setStationButtonHidden(value) {
     document.getElementById("sasasta").hidden = value;
     document.getElementById("iwamotosta").hidden = value;
     document.getElementById("kudansta").hidden = value;
+    document.getElementById("train").hidden = value;
     document.getElementById("backtokeio").hidden = !value;
     document.getElementById("describe-text").hidden = value;
 }
@@ -282,6 +283,12 @@ async function loadKudansta() {
 async function loadiwamotosta() {
     document.getElementById("backtokeio").hidden = false;
     makeCall(camera_peerids["iwamotocho"].peer_id)
+    vue.is_all = false;
+}
+
+async function loadtrain() {
+    document.getElementById("backtokeio").hidden = false;
+    makeCall(camera_peerids["train"].peer_id)
     vue.is_all = false;
 }
 
